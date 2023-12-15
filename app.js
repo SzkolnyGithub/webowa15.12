b6520482c0f7b5eb7ac443c6cc9ea98f0ef833ee
GitMaster55 <144935449+SzkolnyGithub@users.noreply.github.com>
2023-12-15 12:48:12 +0100

    pelna lekcja
    musisz te npm init i npm intall express miec, also pobierz do visual studio code simple express snippets, pan sam mowil zeby to pobrac, giga ulatwia prace. ess zeby miec glowny szablon, esgr dla app.get

--- app.js
+++ app.js
@@ -5,16 +5,31 @@ const app = express();
 let cpu_list = os.cpus();
 let wolnaPamiec = os.freemem();
 let calaPamiec = os.totalmem();
+let procentWolnaPamiec = Math.round((wolnaPamiec / calaPamiec) * 100);
 app.get('/', (req, res) => {
     res.send(cpu_list)
 })
 app.get('/procki', (req, res) => {
     let numer = req.query.numer;
-    res.send(cpu_list[numer]);
+    res.send("User procesora " + numer + " to: " + cpu_list[numer].times.user.toString());
 })
 app.get('/mem', (req, res) => {
-    let procentWolnaPamiec = Math.round((wolnaPamiec / calaPamiec) * 100);
     res.send("Procent wolnej pamięci: " + procentWolnaPamiec + "%");
+})
+app.get('/html', (req, res) => {
+    res.send(`
+    <table border='2'>
+        <tr>
+            <th>Pamięć całkowita</th>
+            <th>Pamięć wolna</th>
+            <th>Procent wolnej pamięci</th>
+        </tr>
+        <tr>
+            <td>${calaPamiec}</td>
+            <td>${wolnaPamiec}</td>
+            <td>${procentWolnaPamiec}%</td>
+        </tr>
+    </table>`)
 })
 
 app.listen(3000, () => {
